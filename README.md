b7f16edc62ce2d18ea6016fde1726272c18f22da
skarolk <skarolkiewicz@gmail.com>
2020-07-19 02:25:41 -0500

    demo finished

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ Hulk vs. HulkBuster Mapping Lab Application
 
 Topic:
 
--The focus of the lesson is the .map() transformation method. So that the lesson is in line with your skillset, you can choose to teach the .map() method using either JavaScript or Java.
+-The focus of the lesson is the .map() transformation method.
 
 Learning Objectives:
 
+-You will be able to explain what differentiates .map() from for loops and .forEach()
 -You will be able to explain the structure and function of .map()
 -You will be able to use map to execute transformations on an array
--You will be able to explain the differences between .forEach() and .map()
 -BONUS: You will be able to explain the advantages of using map in modern frameworks like React
--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
+function renderBruceBanner(char) {
+  return `<center><div><h1 id="bruceName" class="center-text">${char.name}</h1><img id="bruceImage" style="max-width:40%;" src=${char.image}></div></center>`;
+}
+
+function renderTonyStark(char) {
+  return `<center><div><h1 id="tonyName" class="center-text">${char.name}</h1><img id="tonyImage" style="max-width:40%;" src=${char.image}></div></center>`;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  const tonyStarkContainer = document.getElementById("tony-stark-container");
-  const transformTonyStark = document.getElementById("transform-tony-stark");
   const bruceBannerContainer = document.getElementById(
     "bruce-banner-container"
   );
   const transformBruceBanner = document.getElementById(
     "transform-bruce-banner"
   );
-
-  const tonyStark = [
-    {
-      name: "Tony Stark",
-      image:
-        "https://img1.looper.com/img/gallery/audi-may-have-spoiled-who-saves-tony-stark-in-avengers-4/intro-1547480934.jpg",
-    },
-    {
-      name: "Hulk Buster",
-      image:
-        "https://media.wired.com/photos/59325c9d5c4fbd732b5520e7/191:100/pass/hulkbuster-ft.jpg",
-    },
-  ];
+  const tonyStarkContainer = document.getElementById("tony-stark-container");
+  const transformTonyStark = document.getElementById("transform-tony-stark");
 
   const bruceBanner = [
     {
@@ -33,34 +28,21 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
-  tonyStarkContainer.innerHTML = renderTonyStark(tonyStark[0]);
+  const tonyStark = [
+    {
+      name: "Tony Stark",
+      image:
+        "https://img1.looper.com/img/gallery/audi-may-have-spoiled-who-saves-tony-stark-in-avengers-4/intro-1547480934.jpg",
+    },
+    {
+      name: "Hulk Buster",
+      image:
+        "https://www.syfy.com/sites/syfy/files/styles/1200x680_hero/public/wire/legacy/avengers-age-of-ultron-trailer-hulkbuster.jpg",
+    },
+  ];
+
   bruceBannerContainer.innerHTML = renderBruceBanner(bruceBanner[0]);
-
-  transformTonyStark.addEventListener("click", (e) => {
-    // our primary tony stark array
-    console.log("primary tony stark array", tonyStark);
-    const tonyImage = document.getElementById("tonyImage");
-    const tonyName = document.getElementById("tonyName");
-
-    // the transformed tony stark array
-    const transformedTony = tonyStark.map((tony) => {
-      return tonyName.innerHTML === "Tony Stark" ? tonyStark[1] : tonyStark[0];
-    });
-    console.log("map transformed tony stark array", transformedTony);
-    console.log("primary tony stark array", tonyStark);
-
-    // rendering tony stark with map
-    tonyImage.src = tonyStark.map((tony) => {
-      return tonyName.innerHTML === "Tony Stark" ? tonyStark[1] : tonyStark[0];
-    })[0].image;
-    tonyName.innerHTML = `${
-      tonyStark.map((tony) => {
-        return tonyName.innerHTML === "Tony Stark"
-          ? tonyStark[1]
-          : tonyStark[0];
-      })[0].name
-    }`;
-  });
+  tonyStarkContainer.innerHTML = renderTonyStark(tonyStark[0]);
 
   transformBruceBanner.addEventListener("click", (e) => {
     // our primary bruce banner array
@@ -96,12 +78,30 @@ document.addEventListener("DOMContentLoaded", () => {
     bruceImage.src = bruceBanner[0].image;
     bruceName.innerHTML = `${bruceBanner[0].name}`;
   });
+
+  transformTonyStark.addEventListener("click", (e) => {
+    // our primary tony stark array
+    console.log("primary tony stark array", tonyStark);
+    const tonyImage = document.getElementById("tonyImage");
+    const tonyName = document.getElementById("tonyName");
+
+    // the transformed tony stark array
+    const transformedTony = tonyStark.map((tony) => {
+      return tonyName.innerHTML === "Tony Stark" ? tonyStark[1] : tonyStark[0];
+    });
+    console.log("map transformed tony stark array", transformedTony);
+    console.log("primary tony stark array", tonyStark);
+
+    // rendering tony stark with map
+    tonyImage.src = tonyStark.map((tony) => {
+      return tonyName.innerHTML === "Tony Stark" ? tonyStark[1] : tonyStark[0];
+    })[0].image;
+    tonyName.innerHTML = `${
+      tonyStark.map((tony) => {
+        return tonyName.innerHTML === "Tony Stark"
+          ? tonyStark[1]
+          : tonyStark[0];
+      })[0].name
+    }`;
+  });
 });
-
-function renderTonyStark(char) {
-  return `<center><div><h1 id="tonyName" class="center-text">${char.name}</h1><img id="tonyImage" style="max-width:40%;" src=${char.image}></div></center>`;
-}
-
-function renderBruceBanner(char) {
-  return `<center><div><h1 id="bruceName" class="center-text">${char.name}</h1><img id="bruceImage" style="max-width:40%;" src=${char.image}></div></center>`;
-}
